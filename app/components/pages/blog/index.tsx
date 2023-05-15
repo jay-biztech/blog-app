a6f59441119439700b5e921a8d0651b4a1dba4ab
Jay Parmar <jay.parmar@biztechcs.lan>
2023-05-15 11:59:33 +0530

    Add bootstrap

--- app/components/pages/blog/index.tsx
+++ app/components/pages/blog/index.tsx
@@ -3,11 +3,15 @@ import { BlogProps } from './types';
 
 const Blog: React.FC<BlogProps> = (props) => {
   return (
-    <div>
-      <h4>{props.id}</h4>
-      <h4>{props.title}</h4>
-      <p>{props.description}</p>
-      <p>{props.date}</p>
+    <div className="col-md-6">
+      <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div className="col p-4 d-flex flex-column position-static">
+          <strong className="d-inline-block mb-2 text-primary">World</strong>
+          <h3 className="mb-0">{props.title}</h3>
+          <div className="mb-1 text-body-secondary">{props.date}</div>
+          <p className="card-text mb-auto">{props.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- app/components/pages/blogs/index.tsx
+++ app/components/pages/blogs/index.tsx
@@ -5,18 +5,24 @@ import Blog from '../blog';
 const Blogs: React.FC = () => {
   return (
     <div>
-      <h2>Blogs</h2>
-      {blogsMock.map((blog) => {
-        return (
-          <Blog
-            key={blog.id}
-            id={blog.id}
-            title={blog.title}
-            description={blog.description}
-            date={blog.date}
-          />
-        );
-      })}
+      <header className="blog-header lh-1 py-3">
+        <h1>
+          <center>Blogs</center>
+        </h1>
+      </header>
+      <div className="row mb-2">
+        {blogsMock.map((blog) => {
+          return (
+            <Blog
+              key={blog.id}
+              id={blog.id}
+              title={blog.title}
+              description={blog.description}
+              date={blog.date}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
