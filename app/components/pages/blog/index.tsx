a00846fe4e742c319de755826c544600849c0529
Jay Parmar <jay.parmar@biztechcs.lan>
2023-05-15 12:00:10 +0530

    Remove unused code

--- app/components/pages/blog/index.tsx
+++ app/components/pages/blog/index.tsx
@@ -6,7 +6,6 @@ const Blog: React.FC<BlogProps> = (props) => {
     <div className="col-md-6">
       <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div className="col p-4 d-flex flex-column position-static">
-          <strong className="d-inline-block mb-2 text-primary">World</strong>
           <h3 className="mb-0">{props.title}</h3>
           <div className="mb-1 text-body-secondary">{props.date}</div>
           <p className="card-text mb-auto">{props.description}</p>
