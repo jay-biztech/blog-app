3188c97c5186bb37403e0e572c16cea2bb337cba
Jay Parmar <jay.parmar@biztechcs.lan>
2023-05-16 12:20:02 +0530

    Error handling

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -4,6 +4,12 @@ import { BlogProps } from '../components/pages/blog/types';
 
 async function getBlogs() {
   const res = await fetch(`${process.env.BASE_URL}/blogs`);
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
   return res.json();
 }
 
